f43c20e286dcdd9e5a4086d97f0b946e0c40f123
Baklanozavr <abaklan88@gmail.com>
2024-12-03 06:02:25 +0100

    Day02

--- main.py
+++ main.py
@@ -27,9 +27,55 @@ def day_01_2(input_list: list[str]) -> int:
     return sum([a * countB.get(a, 0) for a in listA])
 
 
-if __name__ == '__main__':
-    lines = list_lines_from_file("input/Day01.txt")
+def to_list_of_ints(line: str) -> list[int]:
+    return [int(i) for i in line.split()]
 
-    day_function = day_01_2
+
+def is_report_safe(report: list[int]) -> int:
+    """
+    Return 1 when a report is safe, and 0 when unsafe.
+
+    A report only is safe if both of the following are true:
+    - The levels are either all increasing or all decreasing.
+    - Any two adjacent levels differ by at least one and at most three.
+    """
+    prev_item = report[0]
+    signs = 0
+    for i in range(1, len(report)):
+        delta = report[i] - prev_item
+        if abs(delta) < 1 or abs(delta) > 3:
+            return 0
+        signs += 1 if delta > 0 else -1
+        if abs(signs) != i:
+            return 0
+        prev_item = report[i]
+    return 1
+
+
+def is_report_safe_tolerantly(report: list[int]) -> int:
+    """
+    Same as is_report_safe but tolerates one bad element
+    """
+    for i in range(0, len(report)):
+        sub_report = report[:i] + report[i + 1:]
+        if is_report_safe(sub_report):
+            return 1
+    return 0
+
+
+def day_02_1(input_list: list[str]) -> int:
+    """ How many reports are safe? """
+    return sum([is_report_safe(to_list_of_ints(line)) for line in input_list])
+
+
+def day_02_2(input_list: list[str]) -> int:
+    """ How many reports are safe? """
+    return sum([is_report_safe_tolerantly(to_list_of_ints(line)) for line in input_list])
+
+
+if __name__ == '__main__':
+    lines = list_lines_from_file("input/Day02.txt")
+
+    day_function = day_02_2
 
     print(day_function(lines))
